7ecc9bcc1a9859475d05b66518164bc99497a770
Tigerlilly2931 <acd18a@acu.edu>
2022-10-11 10:53:13 -0500

    Added Profs things

--- app/src/main/java/com/example/volleygsonjson/ItemDetailFragment.java
+++ app/src/main/java/com/example/volleygsonjson/ItemDetailFragment.java
@@ -11,9 +11,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.example.volleygsonjson.placeholder.PlaceholderContent;
 import com.example.volleygsonjson.databinding.FragmentItemDetailBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A fragment representing a single Item detail screen.
@@ -22,6 +29,8 @@ import com.example.volleygsonjson.databinding.FragmentItemDetailBinding;
  * on handsets.
  */
 public class ItemDetailFragment extends Fragment {
+
+    private FloatingActionButton testingFab;
 
     /**
      * The fragment argument representing the item ID that this fragment
@@ -75,6 +84,7 @@ public class ItemDetailFragment extends Fragment {
         mToolbarLayout = rootView.findViewById(R.id.toolbar_layout);
         mTextView = binding.itemDetail;
 
+        testingFab = rootView.findViewById(R.id.fab);
         // Show the placeholder content as text in a TextView & in the toolbar if available.
         updateContent();
         rootView.setOnDragListener(dragListener);
@@ -94,5 +104,41 @@ public class ItemDetailFragment extends Fragment {
                 mToolbarLayout.setTitle(mItem.content);
             }
         }
+        if (testingFab != null)
+        {
+            testingFab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view)
+                {
+                    testAllThatJazz();
+                }
+            });
+        }
+    }
+    private void testAllThatJazz() {
+        String url = "https://api.jsonbin.io/v3/b/5f726a107243cd7e8245d58b";  // THAT should be in a strings.xml file!
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the response string in our convenient existing text view
+                        mTextView.setText("Response is: "+ response);
+                        // NEXT, we need to use GSON to turn that JSON into a model
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // you should drop a breakpoint RIGHT HERE if you need to see the error coming back
+                mTextView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
